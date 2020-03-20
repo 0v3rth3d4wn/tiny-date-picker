9b3802c1713233f3abee95b3f424e1e9780d3822
Ivo Karadzhov <ivo.karadzhov@gmail.com>
2020-03-20 15:03:16 +0200

    Update day-picker view with classnames support
    Update day-picker view with classnames support for year and month

--- src/views/day-picker.js
+++ src/views/day-picker.js
@@ -2,8 +2,8 @@
  * @file Manages the calendar / day-picker view.
  */
 
-import {Key} from '../lib/dom';
-import {now, datesEq, shiftMonth, shiftDay} from '../lib/date-manip';
+import { Key } from '../lib/dom'
+import { now, datesEq, shiftMonth, shiftDay } from '../lib/date-manip'
 
 export default {
   onKeyDown: keyDown,
@@ -17,8 +17,8 @@ export default {
     'dp-cal-month': showMonthPicker,
     'dp-cal-year': showYearPicker,
   },
-  render: render
-};
+  render: render,
+}
 
 /**
  * view renders the calendar (day picker) as an HTML string.
@@ -27,60 +27,84 @@ export default {
  * @returns {string}
  */
 function render(dp) {
-  var opts = dp.opts;
-  var lang = opts.lang;
-  var state = dp.state;
-  var dayNames = lang.days;
-  var dayOffset = opts.dayOffset || 0;
-  var selectedDate = state.selectedDate;
-  var hilightedDate = state.hilightedDate;
-  var hilightedMonth = hilightedDate.getMonth();
-  var today = now().getTime();
+  var opts = dp.opts
+  var lang = opts.lang
+  var state = dp.state
+  var dayNames = lang.days
+  var dayOffset = opts.dayOffset || 0
+  var selectedDate = state.selectedDate
+  var hilightedDate = state.hilightedDate
+  var hilightedMonth = hilightedDate.getMonth()
+  var today = now().getTime()
+  var monthClassNames = opts.monthClassNames
+  var yearClassNames = opts.yearClassNames
 
   return (
     '<div class="dp-cal">' +
-      '<header class="dp-cal-header">' +
-        '<button tabindex="-1" type="button" class="dp-prev">Prev</button>' +
-        '<button tabindex="-1" type="button" class="dp-cal-month">' +
-          lang.months[hilightedMonth] +
-        '</button>' +
-        '<button tabindex="-1" type="button" class="dp-cal-year">' +
-          hilightedDate.getFullYear() +
-        '</button>' +
-        '<button tabindex="-1" type="button" class="dp-next">Next</button>' +
-      '</header>' +
-      '<div class="dp-days">' +
-        dayNames.map(function (name, i) {
-          return (
-            '<span class="dp-col-header">' + dayNames[(i + dayOffset) % dayNames.length] + '</span>'
-          );
-        }).join('') +
-        mapDays(hilightedDate, dayOffset, function (date) {
-          var isNotInMonth = date.getMonth() !== hilightedMonth;
-          var isDisabled = !opts.inRange(date);
-          var isToday = date.getTime() === today;
-          var className = 'dp-day';
-          className += (isNotInMonth ? ' dp-edge-day' : '');
-          className += (datesEq(date, hilightedDate) ? ' dp-current' : '');
-          className += (datesEq(date, selectedDate) ? ' dp-selected' : '');
-          className += (isDisabled ? ' dp-day-disabled' : '');
-          className += (isToday ? ' dp-day-today' : '');
-          className += ' ' + opts.dateClass(date, dp);
+    '<header class="dp-cal-header">' +
+    '<button tabindex="-1" type="button" class="dp-prev">Prev</button>' +
+    '<div class="dp-cal-month-wrapper"> <span>Month</span>' +
+    '<button tabindex="-1" type="button" class="dp-cal-month ' +
+    monthClassNames +
+    '">' +
+    lang.months[hilightedMonth] +
+    '</button>' +
+    '</div>' +
+    '<div class="dp-cal-month-wrapper"> <span>Year</span>' +
+    '<button tabindex="-1" type="button" class="dp-cal-year ' +
+    yearClassNames +
+    '">' +
+    hilightedDate.getFullYear() +
+    '</button>' +
+    '</div>' +
+    '<button tabindex="-1" type="button" class="dp-next">Next</button>' +
+    '</header>' +
+    '<div class="dp-days">' +
+    dayNames
+      .map(function(name, i) {
+        return (
+          '<span class="dp-col-header">' +
+          dayNames[(i + dayOffset) % dayNames.length] +
+          '</span>'
+        )
+      })
+      .join('') +
+    mapDays(hilightedDate, dayOffset, function(date) {
+      var isNotInMonth = date.getMonth() !== hilightedMonth
+      var isDisabled = !opts.inRange(date)
+      var isToday = date.getTime() === today
+      var className = 'dp-day'
+      className += isNotInMonth ? ' dp-edge-day' : ''
+      className += datesEq(date, hilightedDate) ? ' dp-current' : ''
+      className += datesEq(date, selectedDate) ? ' dp-selected' : ''
+      className += isDisabled ? ' dp-day-disabled' : ''
+      className += isToday ? ' dp-day-today' : ''
+      className += ' ' + opts.dateClass(date, dp)
 
-          return (
-            '<button tabindex="-1" type="button" class="' + className + '" data-date="' + date.getTime() + '">' +
-              date.getDate() +
-            '</button>'
-          );
-        }) +
-      '</div>' +
-      '<footer class="dp-cal-footer">' +
-        '<button tabindex="-1" type="button" class="dp-today">' + lang.today + '</button>' +
-        '<button tabindex="-1" type="button" class="dp-clear">' + lang.clear + '</button>' +
-        '<button tabindex="-1" type="button" class="dp-close">' + lang.close + '</button>' +
-      '</footer>' +
+      return (
+        '<button tabindex="-1" type="button" class="' +
+        className +
+        '" data-date="' +
+        date.getTime() +
+        '">' +
+        date.getDate() +
+        '</button>'
+      )
+    }) +
+    '</div>' +
+    '<footer class="dp-cal-footer">' +
+    '<button tabindex="-1" type="button" class="dp-today">' +
+    lang.today +
+    '</button>' +
+    '<button tabindex="-1" type="button" class="dp-clear">' +
+    lang.clear +
+    '</button>' +
+    '<button tabindex="-1" type="button" class="dp-close">' +
+    lang.close +
+    '</button>' +
+    '</footer>' +
     '</div>'
-  );
+  )
 }
 
 /**
@@ -90,91 +114,95 @@ function render(dp) {
  * @param {DatePickerContext} dp
  */
 function keyDown(e, dp) {
-  var key = e.keyCode;
+  var key = e.keyCode
   var shiftBy =
-    (key === Key.left) ? -1 :
-    (key === Key.right) ? 1 :
-    (key === Key.up) ? -7 :
-    (key === Key.down) ? 7 :
-    0;
+    key === Key.left
+      ? -1
+      : key === Key.right
+      ? 1
+      : key === Key.up
+      ? -7
+      : key === Key.down
+      ? 7
+      : 0
 
   if (key === Key.esc) {
-    dp.close();
+    dp.close()
   } else if (shiftBy) {
-    e.preventDefault();
+    e.preventDefault()
     dp.setState({
-      hilightedDate: shiftDay(dp.state.hilightedDate, shiftBy)
-    });
+      hilightedDate: shiftDay(dp.state.hilightedDate, shiftBy),
+    })
   }
 }
 
 function selectToday(e, dp) {
   dp.setState({
     selectedDate: now(),
-  });
+  })
 }
 
 function clear(e, dp) {
   dp.setState({
     selectedDate: null,
-  });
+  })
 }
 
 function close(e, dp) {
-  dp.close();
+  dp.close()
 }
 
 function showMonthPicker(e, dp) {
   dp.setState({
-    view: 'month'
-  });
+    view: 'month',
+  })
 }
 
 function showYearPicker(e, dp) {
   dp.setState({
-    view: 'year'
-  });
+    view: 'year',
+  })
 }
 
 function gotoNextMonth(e, dp) {
-  var hilightedDate = dp.state.hilightedDate;
+  var hilightedDate = dp.state.hilightedDate
   dp.setState({
-    hilightedDate: shiftMonth(hilightedDate, 1)
-  });
+    hilightedDate: shiftMonth(hilightedDate, 1),
+  })
 }
 
 function gotoPrevMonth(e, dp) {
-  var hilightedDate = dp.state.hilightedDate;
+  var hilightedDate = dp.state.hilightedDate
   dp.setState({
-    hilightedDate: shiftMonth(hilightedDate, -1)
-  });
+    hilightedDate: shiftMonth(hilightedDate, -1),
+  })
 }
 
 function selectDay(e, dp) {
   dp.setState({
     selectedDate: new Date(parseInt(e.target.getAttribute('data-date'))),
-  });
+  })
 }
 
 function mapDays(currentDate, dayOffset, fn) {
-  var result = '';
-  var iter = new Date(currentDate);
-  iter.setDate(1);
-  iter.setDate(1 - iter.getDay() + dayOffset);
+  var result = ''
+  var iter = new Date(currentDate)
+  iter.setDate(1)
+  iter.setDate(1 - iter.getDay() + dayOffset)
 
   // If we are showing monday as the 1st of the week,
   // and the monday is the 2nd of the month, the sunday won't
   // show, so we need to shift backwards
   if (dayOffset && iter.getDate() === dayOffset + 1) {
-    iter.setDate(dayOffset - 6);
+    iter.setDate(dayOffset - 6)
   }
 
   // We are going to have 6 weeks always displayed to keep a consistent
   // calendar size
-  for (var day = 0; day < (6 * 7); ++day) {
-    result += fn(iter);
-    iter.setDate(iter.getDate() + 1);
+  for (var day = 0; day < 6 * 7; ++day) {
+    result += fn(iter)
+    iter.setDate(iter.getDate() + 1)
   }
 
-  return result;
+  return result
 }
